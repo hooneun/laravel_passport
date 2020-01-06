6b2415cf76371db62bdef4bdb1b857ad9f627554
김훈 <kimhoon0316@gmail.com>
2020-01-06 18:57:14 +0900

    commit

---
+++ app/Http/Controllers/API/BaseController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BaseController extends Controller
+{
+    /**
+     * @param $result
+     * @param $message
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function sendResponse($result, $message)
+    {
+        $response = [
+            'success' => true,
+            'data' => $result,
+            'message' => $message,
+        ];
+
+        return response()->json($response, 200);
+    }
+
+    public function sendError($error, $errorMessage = [], $code = 404)
+    {
+        $response = [
+            'success' => false,
+            'message' =>$error,
+        ];
+
+        if (!empty($errorMessage)) {
+            $response['data'] = $errorMessage;
+        }
+
+        return response()->json($response, $code);
+    }
+}
